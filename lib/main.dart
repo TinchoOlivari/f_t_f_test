5ac68bcb43fb929461da2c1fa0c1c894d37bc130
Martin Olivari <martin.olivari01@gmail.com>
2024-02-01 18:33:22 -0300

    Create home page

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:f_t_f_test/pages/home_page.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -15,6 +16,7 @@ class App extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.black),
         useMaterial3: true,
       ),
+      home: const HomePage(),
     );
   }
 }
