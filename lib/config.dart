0229dcc9ff3c8995bc34e68f0dd952ae060962b5
Martin Olivari <martin.olivari01@gmail.com>
2024-02-01 18:31:16 -0300

    Create Config class to store url and more

---
+++ lib/config.dart
@@ -0,0 +1,7 @@
+class Config {
+  // Github data
+  static const String owner = 'TinchoOlivari';
+  static const String repo = 'f_t_f_test';
+
+  static const String baseUrl = 'https://api.github.com/repos/$owner/$repo';
+}
