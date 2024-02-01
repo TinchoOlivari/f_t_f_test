c14dcdedd20faebedc29c395c23b5ee91c379214
Martin Olivari <martin.olivari01@gmail.com>
2024-02-01 18:56:48 -0300

    Show commits in commit list page

--- lib/pages/commit_list_page.dart
+++ lib/pages/commit_list_page.dart
@@ -1,4 +1,6 @@
+import 'package:f_t_f_test/provider/commit_provider.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 class CommitListPage extends StatefulWidget {
   const CommitListPage({super.key});
@@ -9,7 +11,33 @@ class CommitListPage extends StatefulWidget {
 
 class _CommitListPageState extends State<CommitListPage> {
   @override
+  void initState() {
+    super.initState();
+    WidgetsBinding.instance.addPostFrameCallback((_) =>
+        Provider.of<CommitProvider>(context, listen: false).getCommitList());
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Commit list"),
+      ),
+      body: Consumer<CommitProvider>(
+        builder: (context, value, child) {
+          final commits = value.commitList;
+
+          return ListView.builder(
+            itemCount: value.commitList.length,
+            itemBuilder: (context, index) {
+              final commit = commits[index];
+              final message = commit.commit?.message ?? "";
+
+              return ListTile(title: Text(message));
+            },
+          );
+        },
+      ),
+    );
   }
 }
